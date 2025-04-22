50ef11ff340a79f60e6047ebe4fa468440b4a71f
Jarno <jarno.veldhuis@gmail.com>
2025-04-22 17:05:27 -0400

    Refactor firebaseAdmin to simplify privateKey handling by commenting out newline character replacement. This change improves code clarity while maintaining functionality.

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -12,7 +12,9 @@ try {
     const credential = process.env.FIREBASE_ADMIN_PRIVATE_KEY
       ? admin.credential.cert({
           projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-          privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle newline characters
+          privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY
+          // ?.replace(/\\n/g, '\n')
+          , // Handle newline characters
           clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
         })
       : admin.credential.applicationDefault();
